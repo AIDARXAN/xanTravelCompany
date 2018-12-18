1f32321846f16c651a2b828c9c83688415127a58
AIDARXAN <32789734+AIDARXAN@users.noreply.github.com>
2018-12-18 22:06:49 +0600

    Add files via upload

--- xan travel company/js/main.js
+++ xan travel company/js/main.js
@@ -32,31 +32,6 @@ function carousel() {
     setTimeout(carousel, 1500); // Change image every 1.5 seconds
 }
 
-$("displayInline").ready(function () {
-    $("#flip1").click(function () {
-        $("#panel1").slideToggle("slow");
-    });
-    $("#flip2").click(function () {
-        $("#panel2").slideToggle("slow");
-    });
-    $("#flip3").click(function () {
-        $("#panel3").slideToggle("slow");
-    });
-    $("#flip4").click(function () {
-        $("#panel4").slideToggle("slow");
-    });
-    $("#flip5").click(function () {
-        $("#panel5").slideToggle("slow");
-    });
-    $("#flip6").click(function () {
-        $("#panel6").slideToggle("slow");
-    });
-    $("#flip7").click(function () {
-        $("#panel7").slideToggle("slow");
-    });
-    $("#flip8").click(function () {
-        $("#panel8").slideToggle("slow");
-    });
-});
+
 
 AOS.init();
